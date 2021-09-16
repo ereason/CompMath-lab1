501e7e1fd5f428f10a17da2ffc8199219ae02f1c
George <george@MacBook-Pro-George.local>
2021-09-17 01:12:40 +0700

    Edit exit condition in half divide method. When b < a - None. Add function call(HDM.start) in main.

--- Pycharm_proj/halfDivideMethod.py
+++ Pycharm_proj/halfDivideMethod.py
@@ -5,8 +5,8 @@ def start(f, sec, ep):  # f - f(x), section - [a,b], ep - epsilon
     middlePoint = (sec['left'] + sec['right']) / 2.0
     value = f(middlePoint)
     sigValue = np.sign(value)   # root = 0, >0 = 1, <0 = -1
-    if ( sec['right']-sec['left'] < 2.0 * ep ):          # exit condition
-        return middlePoint
+    if ( sec['right']-sec['left'] < 2.0 * ep ):    # exit condition
+        return  middlePoint if (sec['right']-sec['left']) > 0  else None    # if b < a - None
 
     if( sigValue == np.sign( f(sec['right']) ) ):
         return start(f, {'left' : sec['left'], 'right' : middlePoint }, ep)
--- Pycharm_proj/main.py
+++ Pycharm_proj/main.py
@@ -1,7 +1,15 @@
 import buildPlot as plot
+import halfDivideMethod
 import numpy as np
 
 def f(t):
     return (t*t) - 20*np.sin(t) - 5
 
 plot.build(f)
+
+print("Look at plot and enter section [a,b]. a < b")
+a, b = map(float, input().split())
+ep = 0.000001
+print("\u03B5 = "+str(ep))
+root = halfDivideMethod.start(f,{'left':a,'right':b},ep)
+print(root)
