09e01da8018870afa38fb27a478910cd6c10e054
George <george@MacBook-Pro-George.local>
2021-09-16 21:26:29 +0700

    Add buildPlot.py, .gitignore files. Add func for building plot

--- Pycharm_proj/main.py
+++ Pycharm_proj/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import buildPlot as plot
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def f(t):
+    return (t*t) - 20*np.sin(t) - 5
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plot.build(f)
