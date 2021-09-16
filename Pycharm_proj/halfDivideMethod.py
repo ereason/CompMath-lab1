6f20e14900badf712c5bfa1b82ee9015d0f44534
George <george@MacBook-Pro-George.local>
2021-09-17 00:45:38 +0700

    Add halfDivideMethod.py file. Third  method in task is done(1 variant). Recursive algorithm

---
+++ Pycharm_proj/halfDivideMethod.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def start(f, sec, ep):  # f - f(x), section - [a,b], ep - epsilon
+
+    middlePoint = (sec['left'] + sec['right']) / 2.0
+    value = f(middlePoint)
+    sigValue = np.sign(value)   # root = 0, >0 = 1, <0 = -1
+    if ( sec['right']-sec['left'] < 2.0 * ep ):          # exit condition
+        return middlePoint
+
+    if( sigValue == np.sign( f(sec['right']) ) ):
+        return start(f, {'left' : sec['left'], 'right' : middlePoint }, ep)
+    else:
+        ( sigValue == np.sign( f(sec['left']) ) )
+        return start(f, {'left' : middlePoint, 'right' : sec['right'] }, ep)
+
+
